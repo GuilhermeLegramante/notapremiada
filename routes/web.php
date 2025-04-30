7c9a7e3da33f8ff09bdff63123c2efef0f510a92
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-04-30 10:19:55 -0300

    Iniciando o projeto

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
+
+Livewire::setScriptRoute(function ($handle) {
+    return Route::get('/notapremiada/public/livewire/livewire.js', $handle);
+});
+
+Livewire::setUpdateRoute(function ($handle) {
+    return Route::post('/notapremiada/public/livewire/update', $handle);
+});
+
+/**
+ * Ao trocar a senha do usuário, o Laravel exige um novo login.
+ * Para isso, é necessário informar a rota de login
+ */
+Route::redirect('/notapremiada/public/admin/login', '/notapremiada/public/admin/login')->name('login');
+
+Route::get('/', function () {
+    return redirect(route('filament.admin.pages.dashboard'));
+});
