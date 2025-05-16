037b9597c8fc08b98a337242e3b04c5f7a7c2c05
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-05-16 14:55:40 -0300

    .

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -12,7 +12,7 @@ class UserObserver
     public function created(User $user): void
     {
         if (! $user->roles()->where('name', 'user_default')->exists()) {
-            $user->assignRole('user_default');
+            $user->assignRole('user_default', 'admin');
         }
     }
 
